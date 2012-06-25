7518f9b8839b0ded37a533c20b97d1fa39ebbc18
Brock Allen <brockallen@gmail.com>
2012-06-25 19:53:48 -0400

    rename to Mvc from MVC

---
+++ IdentityModel/Thinktecture.IdentityModel/Http/Cors/Mvc/MvcConfiguration.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (c) Dominick Baier & Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Thinktecture.IdentityModel.Http.Cors.Mvc
+{
+    public static class MvcConfiguration
+    {
+        static MvcConfiguration()
+        {
+            Configuration = new CorsConfiguration();
+        }
+
+        public static CorsConfiguration Configuration { get; private set; }    
+    }
+}
