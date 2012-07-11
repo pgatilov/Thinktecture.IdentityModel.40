44201e73a1670ea3104a8edc3fc25467078dc745
Dominick Baier <dbaier@leastprivilege.com>
2012-07-11 09:13:45 +0200

    Added MachineKey based session token handler

---
+++ IdentityModel/Thinktecture.IdentityModel/Web/MachineKeyCookieTransform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.IdentityModel.Web;
+using System.Web.Security;
+
+namespace Thinktecture.IdentityModel.Web
+{
+    public class MachineKeyCookieTransform : CookieTransform
+    {
+        public override byte[] Decode(byte[] encoded)
+        {
+            return MachineKey.Decode(Encoding.UTF8.GetString(encoded), MachineKeyProtection.All);
+        }
+
+        public override byte[] Encode(byte[] value)
+        {
+            return Encoding.UTF8.GetBytes(MachineKey.Encode(value, MachineKeyProtection.All));
+        }
+    }
+}
---
+++ IdentityModel/Thinktecture.IdentityModel/Web/MachineKeySessionSecurityTokenHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Web;
+using System.Collections.ObjectModel;
+
+namespace Thinktecture.IdentityModel.Web
+{
+    public class MachineKeySessionSecurityTokenHandler : SessionSecurityTokenHandler
+    {
+        public MachineKeySessionSecurityTokenHandler()
+            : base(CreateTransforms())
+        { }
+
+        public MachineKeySessionSecurityTokenHandler(SecurityTokenCache cache, TimeSpan tokenLifetime)
+            : base(CreateTransforms(), cache, tokenLifetime)
+        { }
+
+        private static ReadOnlyCollection<CookieTransform> CreateTransforms()
+        {
+            return new List<CookieTransform>
+                {
+                    new DeflateCookieTransform(),
+                    new MachineKeyCookieTransform()
+                }.AsReadOnly();
+        }
+    }
+}
