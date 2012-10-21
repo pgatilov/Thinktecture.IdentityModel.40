00dce424a8de3720c22de4e6ac087a65c7cdad6f
Brock Allen <brockallen@gmail.com>
2012-10-21 17:15:03 -0400

    Added "Claims-based Authorization" sample (base)

---
+++ Samples/ClaimsBasedAuthorization/ClaimsBasedAuthorization/Controllers/Api/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityModel.Authorization;
+
+namespace ClaimsBasedAuthorization.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        public string Get()
+        {
+            return "OK";
+        }
+
+        public string Get(int id)
+        {
+            var result = ClaimsAuthorization.CheckAccess("Get", "Customer", id.ToString());
+
+            return "OK " + id.ToString();
+        }
+    }
+}
