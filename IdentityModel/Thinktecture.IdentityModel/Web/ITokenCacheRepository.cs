92b2f3df6e28f44fd59fa669bf2789eb1e4d0103
Brock Allen <brockallen@gmail.com>
2013-02-27 16:15:48 -0500

    migrate passive sesison helpers from IdentityModel 4.5

---
+++ IdentityModel/Thinktecture.IdentityModel/Web/ITokenCacheRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityModel.Web
+{
+    public interface ITokenCacheRepository
+    {
+        void AddOrUpdate(TokenCacheItem item);
+        TokenCacheItem Get(string key);
+        void Remove(string key);
+        void RemoveAllBefore(DateTime date);
+    }
+
+    public class TokenCacheItem
+    {
+        [Key]
+        public string Key { get; set; }
+        public DateTime Expires { get; set; }
+        public byte[] Token { get; set; }
+    }
+}
